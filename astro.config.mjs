b071cc947716287f171c633773a3bbf6245be132
divyavanmahajan <divyavanmahajan@users.noreply.github.com>
2026-01-19 17:53:55 +0100

    chore: configure astro base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // Replace with your site URL (required for sitemap/RSS)
-    site: 'https://example.com',
-    // If hosting on GitHub Pages at a subpath, set 'base' to '/repo-name'
-    // base: '/my-repo', 
+    site: 'https://divyavanmahajan.github.io',
+    base: '/astro-blog',
 });
